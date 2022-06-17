e63355a402d1cd9049c0fad8909d3a4753513871
Jovan Lopez <everest.studio32@gmail.com>
2022-06-17 12:09:55 -0500

    main

--- assets/js/animation-inicio.js
+++ assets/js/animation-inicio.js
@@ -1,5 +1,7 @@
 
 
+var psn = 0;
+
 window.addEventListener('load', function(){
 
     var bannerLeft = document.querySelector('.background > div:first-child');
@@ -9,49 +11,75 @@ window.addEventListener('load', function(){
     bannerRight.style.transform = "translateX(0px)"
     bannerLeft.style.opacity = "100%"
     bannerRight.style.opacity = "100%"
+
+
+    var changeImage = 0;
+
+    changeImage = setInterval(funcionConRetraso, 3000);
+
+
 });
 
+function funcionConRetraso() {
 
+    psn++;
 
-
-
-
-if (screen.width > 1024) {
-
-        var cardProductos = document.querySelectorAll('.card-productos')
-        var cardContenido = document.querySelectorAll('.card-productos-contenido')
+    if(psn > 3)
+        psn = 1
     
-        cardProductos.forEach(function(element){
+    var backgroundImage = document.querySelector('.banner-inicio');
+
+    var urlImage = "url(assets/img/" + "banner-" + psn + ".jpg)"
     
+    //backgroundImage.style.background = "url(assets/img/dos.jpg)";
+
+    backgroundImage.style.background = urlImage;
+    backgroundImage.style.backgroundSize = "cover";
+    backgroundImage.style.backgroundPosition = "center";
+    backgroundImage.style.backgroundAttachment = "fixed";
+
+    console.log(psn)
+
+    
+}
+
+
+
+
+
+
+
+    var cardProductos = document.querySelectorAll('.card-productos')
+    var cardContenido = document.querySelectorAll('.card-productos-contenido')
+
+    cardProductos.forEach(function(element){
+
         element.addEventListener("mouseover", function(event) {
-    
+
             var content = this.querySelector('.card-productos-contenido')
             var content_a = this.querySelector('.card-productos-contenido>a')
-    
+
             content.style.color = "rgba(255, 255, 255, 1)"
             content.style.transform = "translateY(0px)"
-    
+
             content_a.style.color = "rgba(255, 255, 255, 1)"
-    
+
         }, true);
-    
+
         element.addEventListener("mouseout", function(event) {
-    
+
             var content = this.querySelector('.card-productos-contenido')
             var content_a = this.querySelector('.card-productos-contenido>a')
-    
+
             content.style.color = "rgba(255, 255, 255, 0)"
             content.style.transform = "translateY(100px)"
-    
+
             content_a.style.color = "rgba(255, 255, 255, 0)"
-    
+
         }, true);
         
-    
     })
 
-
-}
 
 
 /* cardContenido.forEach(function(element2) {
